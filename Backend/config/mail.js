b8554aaac618d4ed6f323a4d27ccb67bdc997a0b
Nathish M <nathishmuthukumar@gmail.com>
2026-07-17 01:03:15 +0530

    Update mail.js

--- Backend/config/mail.js
+++ Backend/config/mail.js
@@ -1,17 +1,15 @@
-const nodemailer = require("nodemailer");
 const dns = require("dns");
+dns.setDefaultResultOrder("ipv4first");
+
+const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
-  family: 4, // Force IPv4
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
-  },
-  tls: {
-    rejectUnauthorized: false,
   },
 });
 
