a1f63d5e36225ae07073d24db98b4935a6aa5fb9
Nathish M <nathishmuthukumar@gmail.com>
2026-07-17 01:01:01 +0530

    Update mail.js

--- Backend/config/mail.js
+++ Backend/config/mail.js
@@ -8,4 +8,12 @@ const transporter = nodemailer.createTransport({
   },
 });
 
+transporter.verify((error, success) => {
+  if (error) {
+    console.log("SMTP VERIFY ERROR:", error);
+  } else {
+    console.log("SMTP SERVER READY");
+  }
+});
+
 module.exports = transporter;
