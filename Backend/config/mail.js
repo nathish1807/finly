b174ebe03b089a189e26723e1b85759c3536a7b2
Nathish M <nathishmuthukumar@gmail.com>
2026-07-17 01:08:20 +0530

    Update mail.js

--- Backend/config/mail.js
+++ Backend/config/mail.js
@@ -1,16 +1,16 @@
-const dns = require("dns");
-dns.setDefaultResultOrder("ipv4first");
-
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 15000,
+  greetingTimeout: 15000,
+  socketTimeout: 15000,
 });
 
 module.exports = transporter;
